838c06679844445e5736c08b8b7d9a9dee71606b
viceblue <zhuhang0724@gmail.com>
2016-03-12 20:11:55 -0700

    Added files via upload
    part of py code for k means, need to figure out output

---
+++ kmeans.py
@@ -0,0 +1,35 @@
+def Wk(mu, clusters):
+    K = len(mu)
+    return sum([np.linalg.norm(mu[i]-c)**2/(2*len(c)) \
+               for i in range(K) for c in clusters[i]])
+
+def bounding_box(X):
+    xmin, xmax = min(X,key=lambda a:a[0])[0], max(X,key=lambda a:a[0])[0]
+    ymin, ymax = min(X,key=lambda a:a[1])[1], max(X,key=lambda a:a[1])[1]
+    return (xmin,xmax), (ymin,ymax)
+ 
+def gap_statistic(X):
+    (xmin,xmax), (ymin,ymax) = bounding_box(X)
+    # Dispersion for real distribution
+    ks = range(1,10)
+    Wks = zeros(len(ks))
+    Wkbs = zeros(len(ks))
+    sk = zeros(len(ks))
+    for indk, k in enumerate(ks):
+        mu, clusters = find_centers(X,k)
+        Wks[indk] = np.log(Wk(mu, clusters))
+        # Create B reference datasets
+        B = 10
+        BWkbs = zeros(B)
+        for i in range(B):
+            Xb = []
+            for n in range(len(X)):
+                Xb.append([random.uniform(xmin,xmax),
+                          random.uniform(ymin,ymax)])
+            Xb = np.array(Xb)
+            mu, clusters = find_centers(Xb,k)
+            BWkbs[i] = np.log(Wk(mu, clusters))
+        Wkbs[indk] = sum(BWkbs)/B
+        sk[indk] = np.sqrt(sum((BWkbs-Wkbs[indk])**2)/B)
+    sk = sk*np.sqrt(1+1/B)
+    return(ks, Wks, Wkbs, sk)
