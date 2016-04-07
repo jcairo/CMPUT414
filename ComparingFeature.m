0ddde2dfcbd84f7ae9bd4629c5204834a0ba2e91
lin tong <ltong2@ualberta.ca>
2016-04-07 15:37:59 -0700

    updated the variables

--- ComparingFeature.m
+++ ComparingFeature.m
@@ -6,18 +6,28 @@ close all;
 File = fullfile('',FileName); 
 [skel, channels, frameLength] = bvhReadFile(File);
 
+%% get the number of rows in channels and joints
+%  set the number of clusters
+ch_row = size(channels, 1);
+num_joints = size(skel.tree, 2);
+ch_kmean = 5;
+dis_kmean = 4;
+
 %% reduce the channels using PCA
-[coeff, score]=pca(channels);
+[coeff, score] = pca(channels);
+
+%% get the kmeans for channels
+[ch_labels, ch_centroid] = kmeans(channels, ch_kmean);
 
 %% plot the 3D scatter plots based on score
 figure(1);
 scatter3(score(:, 1), score(:, 2), score(:,3));
 
 %% calculating the distance between root and joints
-distance = zeros(1147,37);
-for frames = 1:1147
+distance = zeros(ch_row,num_joints-1);
+for frames = 1:ch_row
     points = bvh2xyz(skel, channels(frames, :));
-    for joints = 2:38
+    for joints = 2:num_joints
         distance(frames, joints-1) = pdist2(points(1, :), points(joints,:));
     end
 end
@@ -25,6 +35,16 @@ end
 %% reduce the matrix using PCA
 pcaDistance = pca(distance);
 
+%% get the kmean for distance
+[dis_labels, dis_centroid] = kmeans(distance, dis_kmean);
+
 %% plot the 3D scatter plots based on pcaDistance
 figure(2);
 scatter3(pcaDistance(:, 1), pcaDistance(:, 2), pcaDistance(:, 3));
+
+%% plot the clutering against time frames
+figure(3);
+plot(ch_labels);
+
+figure(4);
+plot(dis_labels);
